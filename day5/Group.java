5158c15c299a8a999bec226fc804facee60a5e7b
parkjungkwan <pakjkwan@gmail.com>
2021-02-26 17:20:09 +0900

    21-026 완결

--- day5/Group.java
+++ day5/Group.java
@@ -1,5 +1,24 @@
 package day5;
 
-public class Group {
+import java.util.Scanner;
 
+public class Group {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Student[] arr = new Student[3];
+		for(int i=0; i<arr.length; i++) {
+			System.out.println("이름 ?");
+			String name = sc.next();
+			System.out.println("점수 ?");
+			int score = sc.nextInt();
+			Student s1 = new Student();
+			s1.setName(name);
+			s1.setScore(score);
+			arr[i] = s1;
+		}
+		
+		for(int i=0; i<arr.length;i++) {
+			System.out.println("[이름] "+arr[i].getName()+"[점수] "+arr[i].getScore());
+		}
+	}
 }
--- day5/Student.java
+++ day5/Student.java
@@ -1,5 +1,18 @@
 package day5;
 
 public class Student {
-
+	private String name;
+	private int score;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public int getScore() {
+		return this.score;
+	}
 }
